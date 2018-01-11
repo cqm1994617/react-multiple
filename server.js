0d762029ee1ce55f4ac9435ea8499e72c48220d1
淡竹 <danzhu@wacai.com>
2018-01-11 13:16:55 +0800

    express and router

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const config = require('./webpack.config')
 const webpack = require('webpack')
-const WebpackDevServer = require('webpack-dev-server')
-const Koa = require('koa')
-const Router = require('koa-router')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const express = require('express')
+const path = require('path')
 
-const port = 8088
+const port = 8089
 const compiler = webpack(config)
-const app = new Koa()
-const router = new Router()
+const app = express()
 
-router.get()
-
-const devMiddleware = new WebpackDevServer(compiler, {
+const devMiddleware = webpackDevMiddleware(compiler, {
   inline: true,
   disableHostCheck: true,
   contentBase: './',
   hot: true
 })
 
-app.use(devMiddleware).use(Router)
+app.use(devMiddleware)
+
+app.get('/:viewname', function (req, res, next) {
+
+  const viewname = req.params.viewname ? req.params.viewname + '.html' : 'index.html';
+  const filepath = path.join(compiler.outputPath, viewname)
+
+  compiler.outputFileSystem.readFile(filepath, function (err, result) {
+    if (err) {
+      res.send(err)
+      return next(err)
+    }
+    res.set('content-type', 'text/html');
+    res.send(result);
+    res.end();
+  });
+})
 
 app.listen(port, () => {
   console.log('dev start')
